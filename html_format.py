d0c6170e247267fb9838f4c46bde8a2f27812168
tmuzzin <timmuzzin@gmail.com>
2016-03-11 14:42:46 -0700

    updated code to use pytidylib

--- html_format.py
+++ html_format.py
@@ -1,11 +1,30 @@
+
 import glob
-import lxml.html as lh
-from BeautifulSoup import BeautifulSoup as bs
-for f in glob.glob("*.html"):
-	fread = open(f,'r')
-	soup=bs(fread) 
-	fread.close()
-	prettyHTML=soup.prettify()   #prettify the html
-	nf = open(f, 'w')
-	nf.write(prettyHTML)
-	nf.close()
+import sys
+from tidylib import tidy_document, tidy_fragment
+
+options={
+"indent": "auto",
+"indent-spaces": 4,
+"markup": True,
+"output-xml": False,
+"input-xml": False,
+"show-warnings": True,
+"numeric-entities": True,
+"quote-marks": True,
+"quote-nbsp": True,
+"quote-ampersand": False,
+"break-before-br": False,
+"uppercase-tags": False,
+'uppercase-attributes': False
+}
+try:
+    file_type = sys.argv[1]
+except:
+	file_type = "html"
+
+for f in glob.glob("*." + file_type):
+    with open(f) as htmlFragment:
+        htmlFragment, errors = tidy_fragment(htmlFragment.read(),options)
+        f = open(f,'w')
+        f.write(htmlFragment)
